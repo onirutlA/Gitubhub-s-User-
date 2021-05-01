232e854ca6cc847f13b5a8d7bf108d9ab7d24248
onirutlA <alturino001@gmail.com>
2021-05-01 15:14:39 +0700

    Change findViewById to viewBinding

--- app/src/main/java/com/onirutla/githubsuser/view/profile/ProfileFragment.kt
+++ app/src/main/java/com/onirutla/githubsuser/view/profile/ProfileFragment.kt
@@ -6,23 +6,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.onirutla.githubsuser.R
+import com.onirutla.githubsuser.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+    ): View {
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
