3a6ddb8eaf8f5f3bc7145ba8dce47297e1be41d8
onirutlA <alturino001@gmail.com>
2021-05-01 15:13:01 +0700

    Add Room Database

--- app/src/main/java/com/onirutla/githubsuser/data/source/local/db/UserDao.kt
+++ app/src/main/java/com/onirutla/githubsuser/data/source/local/db/UserDao.kt
@@ -1,4 +1,26 @@
 package com.onirutla.githubsuser.data.source.local.db
 
-class UserDao {
+import androidx.room.*
+import com.onirutla.githubsuser.data.source.local.model.UserEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface UserDao {
+    @Insert
+    suspend fun addUsers(users: List<UserEntity>)
+
+    @Insert
+    suspend fun addUser(user: UserEntity)
+
+    @Update
+    suspend fun updateUser(user: UserEntity)
+
+    @Delete
+    suspend fun deleteUser(user: UserEntity)
+
+    @Query("SELECT * FROM users")
+    suspend fun getAllUser(): Flow<List<UserEntity>>
+
+    @Query("SELECT * FROM users WHERE isFavorite = 1")
+    suspend fun getAllUserFavorite(): Flow<List<UserEntity>>
 }
--- app/src/main/java/com/onirutla/githubsuser/data/source/local/db/UserDatabase.kt
+++ app/src/main/java/com/onirutla/githubsuser/data/source/local/db/UserDatabase.kt
@@ -1,4 +1,13 @@
 package com.onirutla.githubsuser.data.source.local.db
 
-class UserDatabase {
+import androidx.room.Database
+import com.onirutla.githubsuser.data.source.local.model.UserEntity
+
+@Database(
+    entities = [UserEntity::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class UserDatabase {
+    abstract fun userDao(): UserDao
 }
